9882952ba072703ac729d827f7d43c337ad9e3dd
unknown <de_meulemeester_c@hotmail.com>
2019-12-11 14:16:07 +0100

    basiclearning,leave me 2it!

--- index.php
+++ index.php
@@ -12,8 +12,26 @@
 </head>
 
 <body>
+    <?php echo "Hello, Echo!"; ?> <br>
+    <?php print "Hello print!"; ?><br>
+    <?php printf ('Hi, printfunction!'); ?><br>
+    <?php echo "Hi, I am learning php from scratch you mofo!" ?><br>
+    <?php $str = "i will be back by "; ?>
 
 
+
+
+<!--
+    1.create input field for products
+    2.create drobdown for foodproducts & drink products
+    3.create a search loop to place products???
+    4.form validation'your email has been send'
+    //from here we do small exercises 
+    //Get-method ===== -->
+     
+<section class="container">
+    <h4 class="center">Order food in restaurant "the Personal Ham Processors"</h4>
+</section>
 
 </body>
 </html>
