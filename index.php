f89fb6c110959f30bda52f44309b37e7c4ff19b4
unknown <de_meulemeester_c@hotmail.com>
2019-12-11 16:50:47 +0100

    add basic L 4:4

--- index.php
+++ index.php
@@ -12,24 +12,26 @@
 </head>
 
 <body>
-    <?php echo "Hello, Echo!"; ?> <br>
-    <?php print "Hello print!"; ?><br>
-    <?php printf ('Hi, printfunction!'); ?><br>
-    <?php echo "Hi, I am learning php from scratch you mofo!"?><br>
+    <?php echo "Hello, Echo!<br/>\n"; ?> 
+    <?php print "Hello print!<br/>\n"; ?>
+    <?php printf ("Hi, printfunction!<br/>\n"); ?>
+    <?php echo "Hi, I am learning php from scratch you mofo!<br/>\n"?>
     <?php 
     $str = "I will be back by ";
     $num = 5;   
         echo $str; echo " ";
         echo $num;
-    ?><br>
+        echo "<br/>\n"; 
+    ?>
    <!---boolean-->
     <?php
-    $late = true;
-    if ($late) 
+    $bar = true;
+    if ($bar) 
     {
         echo "true"; 
     }
     else "fals";
+    echo "<br/>\n"; 
     ?>
     <!---integer_//use_vardump-->
     <?php
@@ -39,8 +41,46 @@
     $zeroPos = 0132 ; //zero positive is used for octal value = 8number (0-7)value (83decimal)
     $hex = 0xAB ; //0x pefix is used in hexidecimal values = 171 decimal (16diffrentsymbols)
     $bin = 01010 ; //bin prifix used for binary values (decimal)
+    ///////////////////////////////floats (decimal numbers)
+    $foo1 = 1.23;
+    $foo2 = 10.0;
+    $bar1 = -INF;
+    $bar2 = NAN;
+    var_dump($foo1,$foo2,$bar1,$bar2);
+    echo "<br/>\n"; 
     var_dump ($negative,$zero,$positive,$zeroPos,$hex,$bin);
-     ?>
+    echo "<br/>\n"; 
+    ?>
+<?php
+    ///////////////////////////////array
+    $products = array(1,2,3); //array of integers
+    $bar = ["Jeej",true,123 => 5]; //short array syntax
+    echo $bar [0]; //returns the first value of the array
+    echo "<br/>\n"; 
+    echo $bar[1]; //returns the second value of the array
+    echo "<br/>\n";  //new line
+    echo $bar[123]; //returns 5
+    //echo $products; //will only call the named array but not the integers unless you define it?
+    #echo $bar[1234]; //  will give an error because 1234 is inaccessable
+    //associative array (contains one or more key's that arnt any integers)
+    $bar = array();
+    $array ["foo"] = "bar";
+    $array ["bar"] = "cool";
+    $array [42]="hello";
+    echo $array["bar"]; // will give guux
+    echo "<br/>\n"; 
+    echo $array[42]; //will give hello
+    echo "<br/>\n"; 
+    //////string (array of characters that can return an individual character out of the string)
+    $joo = "To cool for school";
+    echo $joo;
+    echo "<br/>\n";
+    echo $joo[3]; //print 3th character from array
+
+?>
+
+    
+     
 
 
 
